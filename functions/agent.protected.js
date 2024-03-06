98060e29a5b3c7cb4f6f991294b4811164699ddd
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-06 08:59:19 +0000

    docs: add twilio types

--- functions/agent.protected.js
+++ functions/agent.protected.js
@@ -1,10 +1,11 @@
+//@ts-check
 /**
- * This function should be used to send a message to the agent
- * @param {object} context
- * @param {object} event
- * @param {function} callback
+ * @description This function is used to send a message to the agent
  */
 exports.handler = function (context, event, callback) {
+  /**
+   * @type {import('twilio').Twilio}
+   */
   const client = context.getTwilioClient();
 
   callback(null, 'Message sent');
