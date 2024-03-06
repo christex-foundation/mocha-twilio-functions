464c92287dec951a86f314533e3d8b7db750e202
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-06 15:07:50 +0000

    refactor: variable names

--- functions/agent.protected.js
+++ functions/agent.protected.js
@@ -7,32 +7,32 @@ exports.handler = async function (context, event, callback) {
    * @type {import('twilio').Twilio}
    */
   const client = context.getTwilioClient();
-  const { request, bodySHA256, ...contentVariables } = event;
+  const { request, bodySHA256, ...postBody } = event;
+  const contentTemplateVariables = {
+    1: postBody.amount,
+    2: postBody.currency,
+    3: postBody.address,
+    4: extractPhoneNumber(postBody.sender),
+    to: 'whatsapp:+23273938372', // TODO: Replace with the agent's phone number
+    ...postBody,
+  };
 
-  const message = await sendSingleMessage({ client, context, contentVariables });
-  await registerMessage({ client, context, contentVariables, message });
+  const message = await sendSingleMessage({ client, contentTemplateVariables });
+  await registerMessage({ client, contentTemplateVariables, message });
 
   callback(null, 'Message sent');
 };
 
-async function sendSingleMessage({ client, contentVariables, context }) {
-  contentVariables = {
-    ...contentVariables,
-    1: contentVariables.amount,
-    2: contentVariables.currency,
-    3: contentVariables.address,
-    4: extractPhoneNumber(contentVariables.sender),
-  };
-
+async function sendSingleMessage({ client, contentTemplateVariables }) {
   return client.messages
     .create({
       from: process.env.MESSAGING_SERVICE_SID,
-      contentSid: contentVariables.contentSid,
-      contentVariables: JSON.stringify(contentVariables),
-      to: 'whatsapp:+23273938372', // TODO: Replace with the agent's phone number
+      contentSid: contentTemplateVariables.contentSid,
+      contentVariables: JSON.stringify(contentTemplateVariables),
+      to: contentTemplateVariables.to,
     })
     .then((message) => {
-      console.log(`Message ${message.sid} sent to ${contentVariables.sender} `);
+      console.log(`Message ${message.sid} sent to ${contentTemplateVariables.sender} `);
       return message;
     })
     .catch((err) => {
@@ -41,8 +41,8 @@ async function sendSingleMessage({ client, contentVariables, context }) {
 }
 
 // Function to register the message using Twilio Sync
-async function registerMessage({ client, context, contentVariables, message }) {
-  const phoneNumber = extractPhoneNumber(contentVariables.sender);
+async function registerMessage({ client, contentTemplateVariables, message }) {
+  const phoneNumber = extractPhoneNumber(contentTemplateVariables.sender);
 
   return client.sync.v1
     .services(process.env.SYNC_SERVICE_SID)
@@ -51,8 +51,8 @@ async function registerMessage({ client, context, contentVariables, message }) {
       key: `${phoneNumber}_${message.sid}`, // Use the phone number and message SID as the key
       data: {
         phoneNumber,
-        contentVariables,
-        contentSid: contentVariables.contentSid,
+        contentTemplateVariables,
+        contentSid: contentTemplateVariables.contentSid,
       },
     })
     .then((mapItem) => {
