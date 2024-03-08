548af06be70446def943b28fae3c3a37d7137b56
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-08 12:06:05 +0000

    feat: calculate transaction details

--- functions/tx-details.protected.js
+++ functions/tx-details.protected.js
@@ -11,11 +11,38 @@ const exchangeRates = {
  * @description This function is used to calculate transaction details
  */
 exports.handler = async function (context, event, callback) {
+  const localAmount = getLocalAmount(event.amount);
+  const transferNetwork = getTransferNetwork(event.address);
+
   callback(null, {
-    msg: 'Message sent',
-    transferNetwork: 'SPL',
-    recipient: 'cogoo.sol (7z7a..pKQQ)',
-    localAmount: '1650',
+    transferNetwork,
+    recipient: event.address,
+    localAmount,
     localCurrency: 'SLL',
   });
 };
+
+/**
+ * @description This function is used to get the local amount
+ */
+function getLocalAmount(amount) {
+  return amount * exchangeRates.rates['SLL'];
+}
+
+/**
+ * @description This function is used to get the transfer network
+ */
+function getTransferNetwork(address) {
+  const isSolanaAddress = /\.sol$/.test(address) || /^[1-9A-HJ-NP-Za-km-z]{32,44}$/.test(address);
+  const isTronAddress = /^T[1-9A-HJ-NP-Za-km-z]{33}$/.test(address);
+
+  if (isSolanaAddress) {
+    return 'SPL';
+  }
+
+  if (isTronAddress) {
+    return 'TRX';
+  }
+
+  return '';
+}
