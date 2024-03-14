23585066039e2722b78893e7313f97e4f0f1c95c
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-14 14:06:06 +0000

    fix: rename function

--- create-account-with-seed.mjs
+++ create-account-with-seed.mjs
@@ -2,10 +2,10 @@
 
 import { PublicKey, SystemProgram } from '@solana/web3.js';
 
-async function createAccountWithSeed(publicKey, seed) {
+async function createPubkeyWithSeed(publicKey, seed) {
   return PublicKey.createWithSeed(publicKey, seed, SystemProgram.programId);
 }
 
 const publicKey = new PublicKey('mocaZZMpdRcFpcigpbFyGyqaRiWaGDcbSM1xrM9ZHjo');
-const testKey = await createAccountWithSeed(publicKey, '+23276242792');
+const testKey = await createPubkeyWithSeed(publicKey, '+23276242792');
 console.log(testKey.toBase58());
