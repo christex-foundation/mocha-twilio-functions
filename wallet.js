b5186bdb4c5637c429788f1fcc0210944af52528
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-19 18:24:50 +0000

    feat(fn): create account with seed

--- wallet.js
+++ wallet.js
@@ -19,5 +19,28 @@ import {
   LAMPORTS_PER_SOL,
 } from '@solana/web3.js';
 
+const USDC_DEVNET_MINT = new PublicKey('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU');
+
 const connection = new Connection(clusterApiUrl('devnet'));
 const keypair = await getKeypairFromEnvironment('MOCHA_SECRET_KEY');
+// use whatsapp phone number as seed
+const seed = '23276242792';
+
+const whatsappUserAccount = PublicKey.createWithSeed(keypair.publicKey, seed, TOKEN_PROGRAM_ID);
+
+// create account with seed; assigned to TOKEN_PROGRAM_ID
+const createAccountWithSeedIx = createAccountWithSeed();
+
+function createAccountWithSeed(basePubKey, seed, newAccountPubkey) {
+  const ix = SystemProgram.createAccountWithSeed({
+    fromPubkey: basePubKey,
+    basePubkey: basePubKey,
+    seed,
+    newAccountPubkey,
+    lamports: LAMPORTS_PER_SOL,
+    space: 165, // SPACE FOR TOKEN ACCOUNT .. I THINK
+    programId: TOKEN_PROGRAM_ID,
+  });
+
+  return ix;
+}
