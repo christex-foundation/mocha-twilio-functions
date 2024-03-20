e457e35a3df97fcdc2c429f3ed91444f5bdfa222
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-20 01:15:40 +0000

    feat(scaffold): transfer api

--- transfer.mjs
+++ transfer.mjs
@@ -8,10 +8,36 @@ const app = new Hono();
 
 app.get('/', (c) => c.json('Hello Transfer!'));
 app.post('/', async (c) => {
-  const param = await c.req.json();
-  console.log('🚀 ~ app.post ~ param:', param);
+  const { fromNumber, toNumber, amount } = await c.req.json();
 
-  return c.json('Created transfer!', 201);
+  if (!fromNumber || !toNumber || !amount) {
+    return c.json(
+      {
+        message: 'Invalid request',
+      },
+      400,
+    );
+  }
+
+  const sig = await transfer(fromNumber, toNumber, Number(amount));
+
+  return c.json(
+    {
+      message: 'Transfer successful',
+      signature: sig,
+    },
+    200,
+  );
 });
 
 export default app;
+
+/**
+ * @param {string} fromNumber
+ * @param {string} toNumber
+ * @param {number} amount
+ * @returns {Promise<string>}
+ */
+async function transfer(fromNumber, toNumber, amount) {
+  return '5kpKvUGgioKNSurev8vy4oevq25C9xnc21eFTpQ2DDkRoDkExnotSFXQdqTuGE4UMzMXcD827P1btUybh1HiTLyH';
+}
