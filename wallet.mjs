1baa20fef9630ce2720b4da4da566c12622ddabb
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-20 01:36:34 +0000

    feat(scaffold): validation

--- wallet.mjs
+++ wallet.mjs
@@ -8,9 +8,17 @@ const app = new Hono();
 
 app.get('/', (c) => c.json('Hello Create!'));
 app.post('/', async (c) => {
-  const param = await c.req.json();
-  console.log(process.env.USDC_MINT);
-  console.log('🚀 ~ app.post ~ param:', param);
+  const { phoneNumber } = await c.req.json().catch(() => ({}));
+
+  if (!phoneNumber) {
+    return c.json(
+      {
+        message: 'Invalid request; missing `phoneNumber`',
+      },
+      400,
+    );
+  }
+
   return c.json('Created wallet!', 201);
 });
 
