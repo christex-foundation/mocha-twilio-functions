d50d6c0e26fe7c49cf5b23fd229ecd04d65b3afd
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-25 14:55:47 +0000

    feat: parse amount

--- utils/transfer-spl.mjs
+++ utils/transfer-spl.mjs
@@ -5,6 +5,7 @@ import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 import { getOrCreateUserTokenAccount } from './wallet.mjs';
 import { transfer } from '@solana/spl-token';
 import { Connection, clusterApiUrl } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
 
 dotEnvConfig();
 
@@ -23,12 +24,15 @@ export async function transferUSDC(fromNumber, toNumber, amount) {
   const fromWhatsappUserAccount = await getOrCreateUserTokenAccount(MOCHA_KEYPAIR, fromNumber);
   const toWhatsappUserAccount = await getOrCreateUserTokenAccount(MOCHA_KEYPAIR, toNumber);
 
+  // fix amount
+  let parsedAmount = new BigNumber(amount).multipliedBy(10 ** 6).toNumber();
+
   return await transfer(
     connection,
     MOCHA_KEYPAIR,
     fromWhatsappUserAccount,
     toWhatsappUserAccount,
     MOCHA_KEYPAIR,
-    amount,
+    parsedAmount,
   );
 }
