9490aa373876534684d648703e57be4bc0e95234
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-25 20:01:18 +0000

    feat(wallet): get balance

--- utils/wallet.js
+++ utils/wallet.js
@@ -1,11 +1,15 @@
 //@ts-check
 
+const BASE_URL = 'https://6cd7-196-216-220-211.ngrok-free.app/api/v1/wallet';
+
 /**
  * @description Function to fetch the wallet balance
  * @param {string} phoneNumber
  */
 async function fetchWalletBalance(phoneNumber) {
-  return 0;
+  const response = await fetch(`${BASE_URL}?phoneNumber=${phoneNumber}`).then((res) => res.json());
+
+  return response.balance;
 }
 
 module.exports = { fetchWalletBalance };
