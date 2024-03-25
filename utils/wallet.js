810931dec5a2ded3ba1a8cec388cda058fc71538
Colin Ogoo <ogoo.colin@gmail.com>
2024-03-25 20:14:33 +0000

    fix: path param instead of query param

--- utils/wallet.js
+++ utils/wallet.js
@@ -7,7 +7,7 @@ const BASE_URL = 'https://6cd7-196-216-220-211.ngrok-free.app/api/v1/wallet';
  * @param {string} phoneNumber
  */
 async function fetchWalletBalance(phoneNumber) {
-  const response = await fetch(`${BASE_URL}?phoneNumber=${phoneNumber}`).then((res) => res.json());
+  const response = await fetch(`${BASE_URL}/${phoneNumber}`).then((res) => res.json());
 
   return response.balance;
 }
